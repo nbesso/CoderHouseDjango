06e04d8b3077587a0ce0d79c82f7ac702be9e211
Natali Besso <nbesso@koandina.com>
2022-02-03 10:39:42 -0300

    Create diferents templates

--- Proyecto1/urls.py
+++ Proyecto1/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from Proyecto1.views import saludo, segunda_vista, dia_de_hoy, mi_nombre, template1
+from Proyecto1.views import saludo, segunda_vista, dia_de_hoy, mi_nombre, template1, template2, template3, template4, template5
 
 urlpatterns = [
     path('saludo/', saludo),
@@ -8,5 +8,9 @@ urlpatterns = [
     path('diadehoy/', dia_de_hoy),
     path('prueba4/<nombre>', mi_nombre),
     path('template1/', template1),
+    path('template2/', template2),
+    path('template3/', template3),
+    path('template4/<nombre>', template4),
+    path('template5/', template5),
     path('admin/', admin.site.urls),
 ]
--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from datetime import datetime
@@ -24,3 +25,20 @@ def mi_nombre(request, nombre):
 
 def template1(request):
     return render(request, "template1.html")
+
+def template2(request):
+    return render(request, "template2.html")
+
+def template3(request):
+    curso = "Python"
+    context = {"curso": "python"}
+    return render(request, "template3.html", context)
+
+def template4(request, nombre):
+    context = {"nombre": nombre}
+    return render(request, "template4.html", context)
+
+def template5(request):
+    context = {"lista": [1,2,3,4]}
+    return render(request, "template5.html", context)
+
